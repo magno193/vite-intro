4bf2dce3c603b2d42ea0bf3cc8c9b96b83087bc8
Alexandre Ferreira <alexmlf93@gmail.com>
2021-09-24 14:09:59 -0300

    Introdução de vite com vanilla js

---
+++ main.ts
@@ -0,0 +1,8 @@
+import { debounce } from 'lodash-es';
+import './style.css';
+console.log(debounce);
+
+document.querySelector('#app').innerHTML = `
+  <h1>Hello Vite!</h1>
+  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+`
